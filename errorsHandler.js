c4b2f35187611b0b322aecf7348dd2d160d0c1db
lachero-work <asaf@gwumkt.com>
2020-08-20 22:48:55 +0300

    added open log url feature

--- errorsHandler.js
+++ errorsHandler.js
@@ -23,7 +23,21 @@ const throwError = err => {
     };
 };
 
-exports.errorsHandler = async ({ awsRequestId: id, functionName }, input, output) => {
+const generateContextData = ({ awsRequestId: id, functionName, logGroupName, logStreamName }) => {
+    
+    let url = 'https://us-east-2.console.aws.amazon.com/cloudwatch/home?region=us-east-2#logsV2:log-groups/log-group/';
+    const groupName = logGroupName.replace(/\//g, '$252F');
+    const streamName = logStreamName.replace(/\$/g, '$2524').replace(/\//g, '$252F').replace(/\[/g, '$255B').replace(/\]/g, '$255D');
+    url += `${groupName}/log-events/${streamName}`;
+    
+    return {
+        id,
+        functionName,
+        url,
+    };
+};
+
+exports.errorsHandler = async (context, input, output) => {
     // printing the full error object
     console.log(output);
     
@@ -31,8 +45,7 @@ exports.errorsHandler = async ({ awsRequestId: id, functionName }, input, output
     const params = {
         FunctionName: 'errorsHandler',
         InvokeArgs: JSON.stringify({
-            id,
-            functionName,
+            ...generateContextData(context),
             input,
             output: error,
         }),
